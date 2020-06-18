487778003318acabf57dbd92d2459aa43eb44f40
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-06-18 16:11:56 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Avis_Concession_Automobile</title>
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- FONTAWESOME -->
+  <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
+
+  <!-- BOOTSTRAP-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- CSS THEME -->
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+  <!-- Start Header -->
+  <header class="image-header">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light opacity">
+      <span><img src="images/header/hamburger-icon.png" alt="hamburger-icon" class="hamburger-icon"></span>
+      <div class="logo text-center mt-4">
+        <a href="#"><img src="images/header/logo.png" alt="logo"></a>
+      </div>
+    </nav>
+    <!-- End navbar -->
+    <div class="d-flex justify-content-end">
+      <h2 class="titre-header text-uppercase text-white pr-2">notre expertise</h2>
+    </div>
+  </header>
+  <!-- End Header -->
