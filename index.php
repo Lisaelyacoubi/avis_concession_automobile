3ad2f641c1d8090105488689cefc050cd9c7ee0d
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-06-19 11:56:43 +0200

    [done] font size paragraphe

--- index.php
+++ index.php
@@ -3,28 +3,28 @@ include('header.php');
  ?>
 
 <!-- Start Qui sommes nous -->
-<section class="bg-qui-sommes-nous  ">
+<section class="bg-qui-sommes-nous">
   <div class="container">
     <div class="row">
-      <div class="col-lg-6">
+      <div class="col-lg-5">
         <h2 class="titre-qui-sommes-nous text-white">Qui sommes-nous ?</h2>
-        <p class="text-white">Sport Design Automobiles est une jeune société spécialisée
+        <p class="text-white paragraphe-size-qui-sommes-nous">Sport Design Automobiles est une jeune société spécialisée
         dans la recherche personnalisée de véhicule toute marque,
         neuf ou occasion, dotée de professionnels de l’automobile
         cûmulant plus de 10 ans d’expérience.
         </p>
-        <p class="text-white">Notre priorité ? La satisfaction du client. Nous mettons tout
+        <p class="text-white paragraphe-size-qui-sommes-nous">Notre priorité ? La satisfaction du client. Nous mettons tout
         en œuvre pour vous trouver LE véhicule qui vous correspond
         en fonction de vos critères.
         </p>
-        <p class="text-white">Notre spécialité ? La recherche personnalisée de véhicule neuf
+        <p class="text-white paragraphe-size-qui-sommes-nous">Notre spécialité ? La recherche personnalisée de véhicule neuf
         ou occasion, toute gamme et toute marque confondue.
         </p>
-        <p class="text-white">N’hésitez pas à contacter nos commerciaux qui se feront un
+        <p class="text-white paragraphe-size-qui-sommes-nous">N’hésitez pas à contacter nos commerciaux qui se feront un
         plaisir de vous renseigner.
         </p>
       </div>
-      <div class="col-lg-6 hover-presentation bg-photo-presentation text-center">
+      <div class="col-lg-7 hover-presentation bg-photo-presentation text-center">
           <div class="text-center overlay-presentation">
             <h3 class="d-inline-block text-uppercase text-white text-center">l'avis de nos clients</h2>
               <img src="images/Qui_Sommes_Nous/car.png" alt="car" class="car-icon">
@@ -125,7 +125,7 @@ include('header.php');
 </section>
 <!-- End Qui sommes nous -->
 
-<!-- Start Qui sommes nous -->
+<!-- Start  -->
 <section>
 
 </section>
