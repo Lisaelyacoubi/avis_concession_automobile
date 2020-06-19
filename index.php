e6e68b01f3be174445c0a66603d65f594bd75420
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-06-19 11:48:08 +0200

    [done] footer

--- index.php
+++ index.php
@@ -3,125 +3,132 @@ include('header.php');
  ?>
 
 <!-- Start Qui sommes nous -->
-<section class="bg-qui-sommes-nous">
-  <div class="row ">
-    <div class="col-lg-6">
-      <h2 class="titre-qui-sommes-nous text-white">Qui sommes-nous ?</h2>
-      <p class="text-white">Sport Design Automobiles est une jeune société spécialisée
-      dans la recherche personnalisée de véhicule toute marque,
-      neuf ou occasion, dotée de professionnels de l’automobile
-      cûmulant plus de 10 ans d’expérience.
-      </p>
-      <p class="text-white">Notre priorité ? La satisfaction du client. Nous mettons tout
-      en œuvre pour vous trouver LE véhicule qui vous correspond
-      en fonction de vos critères.
-      </p>
-      <p class="text-white">Notre spécialité ? La recherche personnalisée de véhicule neuf
-      ou occasion, toute gamme et toute marque confondue.
-      </p>
-      <p class="text-white">N’hésitez pas à contacter nos commerciaux qui se feront un
-      plaisir de vous renseigner.
-      </p>
-    </div>
-    <div class="col-lg-6 bg-photo-presentation hover-presentation text-center">
-      <h3 class="d-inline-block text-uppercase text-white text-center">l'avis de nos clients</h2>
-        <img src="images/Qui_Sommes_Nous/car.png" alt="car" class="car-icon">
-        <div class="text-center  overlay-presentation">
-          <h4 class="text-uppercase text-white">qualité de notre accueil</h4>
-            <ul class="d-flex flex-row justify-content-center">
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
-              </li>
-            </ul>
-          <h4 class="text-uppercase text-white text-center">nos infrastructures</h4>
-            <ul class="d-flex flex-row justify-content-center">
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
-              </li>
-            </ul>
-          <h4 class="text-uppercase text-white">notre professionnalisme</h4>
-            <ul class="d-flex flex-row justify-content-center">
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
-              </li>
-            </ul>
-          <h4 class="text-uppercase text-white text-center">suivi de commande</h4>
-            <ul class="d-flex flex-row justify-content-center">
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
-              </li>
-            </ul>
-          <h4 class="text-uppercase text-white text-center">véhicule à la livraison</h4>
-            <ul class="d-flex flex-row justify-content-center">
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
-              </li>
-              <li>
-                <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
-              </li>
-            </ul>
+<section class="bg-qui-sommes-nous  ">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h2 class="titre-qui-sommes-nous text-white">Qui sommes-nous ?</h2>
+        <p class="text-white">Sport Design Automobiles est une jeune société spécialisée
+        dans la recherche personnalisée de véhicule toute marque,
+        neuf ou occasion, dotée de professionnels de l’automobile
+        cûmulant plus de 10 ans d’expérience.
+        </p>
+        <p class="text-white">Notre priorité ? La satisfaction du client. Nous mettons tout
+        en œuvre pour vous trouver LE véhicule qui vous correspond
+        en fonction de vos critères.
+        </p>
+        <p class="text-white">Notre spécialité ? La recherche personnalisée de véhicule neuf
+        ou occasion, toute gamme et toute marque confondue.
+        </p>
+        <p class="text-white">N’hésitez pas à contacter nos commerciaux qui se feront un
+        plaisir de vous renseigner.
+        </p>
+      </div>
+      <div class="col-lg-6 hover-presentation bg-photo-presentation text-center">
+          <div class="text-center overlay-presentation">
+            <h3 class="d-inline-block text-uppercase text-white text-center">l'avis de nos clients</h2>
+              <img src="images/Qui_Sommes_Nous/car.png" alt="car" class="car-icon">
+            <h4 class="text-uppercase text-white">qualité de notre accueil</h4>
+              <ul class="d-flex flex-row justify-content-center">
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
+                </li>
+              </ul>
+            <h4 class="text-uppercase text-white text-center">nos infrastructures</h4>
+              <ul class="d-flex flex-row justify-content-center">
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
+                </li>
+              </ul>
+            <h4 class="text-uppercase text-white">notre professionnalisme</h4>
+              <ul class="d-flex flex-row justify-content-center">
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
+                </li>
+              </ul>
+            <h4 class="text-uppercase text-white text-center">suivi de commande</h4>
+              <ul class="d-flex flex-row justify-content-center">
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
+                </li>
+              </ul>
+            <h4 class="text-uppercase text-white text-center">véhicule à la livraison</h4>
+              <ul class="d-flex flex-row justify-content-center">
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-black.png" alt="star-black" class="mr-4"></span>
+                </li>
+                <li>
+                  <span><img src="images/Qui_Sommes_Nous/star-white.png" alt="star-white" ></span>
+                </li>
+              </ul>
+            </div>
           </div>
-    </div>
+        </div>
   </div>
 </section>
 <!-- End Qui sommes nous -->
+
+<!-- Start Qui sommes nous -->
+<section>
+
+</section>
 
 
   <?php
